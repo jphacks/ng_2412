e2138200b798e6d6e77e01b746aedd5eb0448bb6
shun199511 <shun100noheya@gmail.com>
2024-10-27 12:24:47 +0900

    タイトルからテンプレートへの画面遷移

--- app/src/main/java/com/example/myapplication/title.java
+++ app/src/main/java/com/example/myapplication/title.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.content.Intent;
+
 
 public class title extends AppCompatActivity {
 
@@ -19,6 +22,16 @@ public class title extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //ボタンを押した時
+        findViewById(R.id.start_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //インテントの追加（これが遷移用ロジックです）
+                Intent intent = new Intent(title.this, TemplateActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
